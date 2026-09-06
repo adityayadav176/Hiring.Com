fd7b26212a459bd17ef5d5a2b612517e3da5c28b
Aditya Yadav <217367477+adityayadav176@users.noreply.github.com>
2026-09-06 14:00:21 +0530

    intitlized Hiring Application frountend using Vite utility and implement react-router-dom

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<h1 className="text-3xl text-white items-center justify-center flex">Home</h1>}/>
+            <Route path="/Login" element={<h1 className="text-3xl text-white items-center justify-center flex">Login</h1>}/>
+            <Route path="/signup" element={<h1 className="text-3xl text-white items-center justify-center flex">Signup</h1>}/>
+            <Route path="/admin/dashboard" element={<h1 className="text-3xl text-white items-center justify-center flex">Admin</h1>}/>
+            <Route path="/recruiter" element={<h1 className="text-3xl text-white items-center justify-center flex">Recruiter</h1>}/>
+            <Route path="/seeker" element={<h1 className="text-3xl text-white items-center justify-center flex">Seekar</h1>}/>
+            <Route path="*" element={<h1 className="text-3xl text-white items-center justify-center flex">404 - Page Not Found</h1>}/>
+        </Routes>
+    );
+};
+
+export default AppRoutes;
