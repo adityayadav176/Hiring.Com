23cc3415b701e0b6338bdc2e5cd85e3d0018b8a3
Aditya Yadav <217367477+adityayadav176@users.noreply.github.com>
2026-09-09 22:22:53 +0530

    implement handleBlockCompay in frontend

---
+++ src/context/AdminContext.js
@@ -0,0 +1,36 @@
+import { createContext } from "react";
+
+export const AdminContext = createContext();
+
+const AdminProvider = ({children}) => {
+
+    const API_URL = import.meta.env.VITE_API_URL;
+
+    const blockCompany = async(companyId) => {
+        try {
+            const response = await fetch(`${API_URL}/admin/companies/${companyId}/block`, {
+                credentials: true,
+                method: "PATCH"
+            })
+    
+            const data = await response.json();
+    
+            if(!response.ok) {
+                throw new Error(data.message);
+            }
+    
+            console.log("Company Block Successfully");
+        } catch (error) {
+            console.log(error);
+            alert(error.message);
+        }
+    }
+
+    return (
+        <AdminContext.Provider value={blockCompany}>
+            {children}
+        </AdminContext.Provider>
+    )
+}
+
+export default AdminProvider;
