31b26221466b502efa9ee540840ae46e39d183f6
Aditya Yadav <217367477+adityayadav176@users.noreply.github.com>
2026-09-11 14:45:06 +0530

    Wrap Whole App In AppContextProvider.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import TitleProvider from "./context/TitleContext.jsx";
+import AppContextProvider from './context/AppContextProvider';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <TitleProvider>
+    <AppContextProvider>
         <App />
-    </TitleProvider>
+    </AppContextProvider>
     </BrowserRouter>
   </StrictMode>
 );
